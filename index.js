a1acf22fc240211dbc18be154af0af932f2ca596
Micky Wolf <mickywolf@utexas.edu>
2018-07-02 15:46:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string) {
   return "I can't hear you!";
   }
 
-  elseif (string.toUpperCase() === string) {
+  elseif (string.toUpperCase() === string); {
   return "YES INDEED!";
   }
   else (string === "I love you, Grandma.") {
